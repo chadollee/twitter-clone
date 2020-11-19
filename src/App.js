29fb49d7663181b7cb6b8c11c118880ed3323a73
chad <chadollee@protonmail.com>
2020-11-19 15:54:32 -0500

    made CreateTweet component with a form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 //Import components
 import Tweet from "./components/Tweet";
+import CreateTweet from "./components/CreateTweet";
 
 function App() {
   return (
     <div>
       <h1>Hello React</h1>
-    </div>
+      <CreateTweet />
+      <Tweet />
+    </div> 
   );
 }
 
